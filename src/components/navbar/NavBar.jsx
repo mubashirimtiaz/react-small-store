1dc566616549bd97b574b8f05179e9dd80268b27
mubashirimtiaz <mubashir.imtiaz123@gmail.com>
2021-01-11 21:08:50 +0500

    updated cart styling

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,42 +1,50 @@
 import React, { useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { CartContext } from "../../context/cartContext";
+import "./navbar.css";
 
 const Navbar = () => {
   const { cartProducts } = useContext(CartContext);
   return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand  font-weight-bold" to="/">
-          Small-Store
-        </Link>
-
-        <ul className="navbar-nav ml-auto mt-2  mt-lg-0">
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="/" end>
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="about">
-              About
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="active" to="contact">
-              Contact
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              className="nav-link bg-danger p-2 ml-md-3 rounded text-light font-weight-bold"
-              activeClassName="active"
-              to="cart"
-            >
-              Cart <sup>{cartProducts.length}</sup>
-            </NavLink>
-          </li>
-        </ul>
+    <nav className="navbar-dark fixed-top bg-dark py-2">
+      <div className="container d-md-flex justify-content-between align-items-center d-block">
+        <div className="text-center">
+          <Link className="py-3 lead  text-light font-weight-bold" to="/">
+            Small-Store
+          </Link>
+        </div>
+        <div>
+          <ul className="ml-auto text-center my-auto p-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/" end>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link bg-danger p-2 ml-md-3 rounded text-light font-weight-bold"
+                activeClassName="active"
+                to="cart"
+              >
+                Cart <sup>{cartProducts.length}</sup>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/components/watch_product/WatchProducts.jsx
+++ src/components/watch_product/WatchProducts.jsx
@@ -1,15 +1,29 @@
 import React, { useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { CartContext } from "../../context/cartContext";
 import { ProductContext } from "../../context/productsContext";
 
 const WatchProduct = () => {
   const { prodID } = useParams();
   const navigate = useNavigate();
   const { products } = useContext(ProductContext);
-  const handleClick = () => {
+  const { dispatch } = useContext(CartContext);
+  const handleNavigation = () => {
     navigate("/", { replace: true });
   };
   const watchedProduct = products.find(({ id }) => id === prodID);
+
+  const handleClick = (product) => {
+    dispatch({
+      type: "ADD_PRODUCT",
+      payload: {
+        id: product.id,
+        name: product.fields.name,
+        imageUrl: product.fields.image[0].url,
+        price: product.fields.price,
+      },
+    });
+  };
 
   if (!watchedProduct) {
     return (
@@ -17,7 +31,7 @@ const WatchProduct = () => {
         <div className="row">
           <div className="col-md-12 text-center">
             <p className="display-4">Product not Found</p>
-            <button className="btn btn-outline-info" onClick={handleClick}>
+            <button className="btn btn-outline-info" onClick={handleNavigation}>
               Go back to Home
             </button>
           </div>
@@ -43,7 +57,12 @@ const WatchProduct = () => {
               </h5>
               <p className="card-text">${watchedProduct.fields.price}</p>
               <p className="card-text text-right">
-                <button className="btn btn-outline-danger">Add to Cart</button>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => handleClick(watchedProduct)}
+                >
+                  Add to Cart
+                </button>
               </p>
             </div>
           </div>
